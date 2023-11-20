de61f3f421e36ff68a730feae8a346143178eace
Haviv1901 <98889657+Haviv1901@users.noreply.github.com>
2023-11-20 21:31:58 +0200

    fixed numerical tree scanning using postorder.

--- compiler/InstructionWriting.c
+++ compiler/InstructionWriting.c
@@ -48,17 +48,15 @@ void convertASTToASM(ASTNode* tree, const char* fileName)
 {
 	char* asmPath = NULL;
 	asmPath = (char*)calloc(strlen(fileName) + 5, sizeof(char));
-	if (asmPath == NULL)
-	{
-		printf("could not open file: %s\n", fileName);
-		return;
-	}
+
 	strcpy(asmPath, fileName);
 	strcat(asmPath, ".asm");
 	FILE* asmFile = openFile(asmPath, "w");
+
 	copyBoneFile(asmFile, FIRST); // copy first half of the basic functions and start of main
 
-	writeInstructions(tree, asmFile); // write instructions from the ast
+	writeBranch(tree, asmFile); // write instructions from the ast
+
 	copyBoneFile(asmFile, SECOND); // copy the second half
 	free(asmPath);
 	fclose(asmFile);
@@ -68,27 +66,46 @@ void convertASTToASM(ASTNode* tree, const char* fileName)
 
 
 /*
-writeInstructions: this function will write all instructions to the asm file after the first bone was added
+writeBranch: this function will write all instructions to the asm file after the first bone was added
 input: the tree, and the asm file
 output: non
 */
-void writeInstructions(ASTNode* tree, FILE* asmFile)
+void writeBranch(ASTNode* tree, FILE* asmFile)
 {
 	if (tree == NULL)// if the tree is null do nothing
 	{
 		return;
 	}
-	Token* currentToken = tree->children[1]->token;// getting the command part of this node
+
+	Token* currentToken;
+	ASTNode* next = NULL;
+	if(tree->token == NULL)
+	{
+		currentToken = tree->children[EXPRESSION]->token;// getting the command part of this node
+		next = tree->children[NEXT];
+		tree = tree->children[EXPRESSION];
+	}
+	else
+	{
+		currentToken = tree->token;// getting the command part of this node
+	}
+	
+
 	if (currentToken->type == NUM || currentToken->type == ADD || currentToken->type == SUB || currentToken->type == MUL || currentToken->type == DIV)// checking for numeric branch
 	{
-		writeNumericBranch(tree->children[1], asmFile);
+		writeNumericBranch(tree, asmFile);
+		fprintf(asmFile, "pop eax\n");
 	}
 	else if (currentToken->type == PRINT)// checking for function branch
 	{
-		writeFunctionBranch(tree->children[1], asmFile);
+		writeFunctionBranch(tree, asmFile);
 	}
-	writeInstructions(tree->children[0], asmFile);
-	return;
+
+	if(next != NULL)
+	{
+		writeBranch(next, asmFile);
+	}
+	
 }
 
 
@@ -100,28 +117,28 @@ output: non
 */
 void writeNumericBranch(ASTNode * branch, FILE * asmFile)
 {
-	if (branch->token->type == NUM)// checking if its an only number
+	if(branch->token->type == NUM)
 	{
-		fprintf(asmFile, "mov eax, %d\n", *(int*)branch->token->value);
+		fprintf(asmFile, "push %d\n", *(int*)branch->token->value);
 		return;
 	}
-	if (branch->children[0]->token->type == NUM && branch->children[1]->token->type == NUM)//if its the deepest root
-	{
-		fprintf(asmFile, "mov eax, %d\n", *(int*)branch->children[0]->token->value);//writing the first put
-		writeNumericInstruction(branch->token, branch->children[1]->token, asmFile);//writing instructions
-	}
-	else
-	{
-		writeNumericBranch(branch->children[1], asmFile);// going to the deepest root first
-		writeNumericInstruction(branch->token, branch->children[0]->token, asmFile);//writing instruction
 
-	}
-	return;
+	// token is an operator:
 
+	writeNumericBranch(branch->children[0], asmFile);
+	writeNumericBranch(branch->children[1], asmFile);
 
+	fprintf(asmFile, "pop ebx\n"); // put into ebx the solution for the right side if the tree
+	fprintf(asmFile, "pop eax\n"); // put into eax the solution for the left side if the tree
 
+	writeNumericInstruction(branch->token, asmFile); // write the instruction
+
+	// push the final solution to the stack
+	fprintf(asmFile, "push eax\n");
 
 }
+
+
 
 
 
@@ -130,26 +147,53 @@ writeNumericInstruction: this function will get the operand and value of a numer
 input: the tokens of the operand and value, and the asm file
 output: non
 */
-void writeNumericInstruction(Token* operand, Token* value, FILE* asmFile)
+void writeNumericInstruction(Token* operand, FILE* asmFile)
 {
 	if (operand->type == ADD)//checking for the correct operand, and preforming it on eax
 	{
-		fprintf(asmFile, "add eax, %d\n", *(int*)value->value);
+		fprintf(asmFile, "add eax, ebx\n");
 	}
 	else if (operand->type == SUB)
 	{
-		fprintf(asmFile, "sub eax, %d\n", *(int*)value->value);
+		fprintf(asmFile, "sub eax, ebx\n");
 	}
 	else if (operand->type == MUL)
 	{
-		fprintf(asmFile, "imul eax, %d\n", *(int*)value->value);
+		fprintf(asmFile, "imul eax, ebx\n");
 	}
-	else if (operand->type == DIV)//using ebx sience idiv cannot use immidiates
+	else if (operand->type == DIV)//using ebx seince idiv cannot use immidiates
 	{
-		fprintf(asmFile, "mov ebx, %d\nidiv ebx\n", *(int*)value->value);
+		fprintf(asmFile, "idiv ebx\n");
 	}
 	return;
 }
+
+//
+///*
+//writeNumericInstruction: this function will get the operand and value of a numeric type, and write the correct instruction
+//input: the tokens of the operand and value, and the asm file
+//output: non
+//*/
+//void writeNumericInstruction(Token* operand, Token* value, FILE* asmFile)
+//{
+//	if (operand->type == ADD)//checking for the correct operand, and preforming it on eax
+//	{
+//		fprintf(asmFile, "add eax, %d\n", *(int*)value->value);
+//	}
+//	else if (operand->type == SUB)
+//	{
+//		fprintf(asmFile, "sub eax, %d\n", *(int*)value->value);
+//	}
+//	else if (operand->type == MUL)
+//	{
+//		fprintf(asmFile, "imul eax, %d\n", *(int*)value->value);
+//	}
+//	else if (operand->type == DIV)//using ebx sience idiv cannot use immidiates
+//	{
+//		fprintf(asmFile, "mov ebx, %d\nidiv ebx\n", *(int*)value->value);
+//	}
+//	return;
+//}
 
 
 
@@ -165,7 +209,7 @@ void writeFunctionBranch(ASTNode* branch, FILE* asmFile)
 	if (branch->token->type == PRINT)// checking for print (in the futer there will be )
 	{
 		writeNumericBranch(branch->children[0], asmFile);
-		fprintf(asmFile, "push eax\ncall print_number_signed\n");
+		fprintf(asmFile, "call print_number_signed\n");
 	}
 	return;
 }
